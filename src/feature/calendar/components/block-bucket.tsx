5a9be9ff1bd320e954aa7b63be0e71d57a9d6092
Taewoong Heo <chamjoeun0111@gmail.com>
2026-02-02 14:11:51 +0900

    feat(maps): 모바일 뷰 지도 레이아웃 변경

--- src/feature/calendar/components/block-bucket.tsx
+++ src/feature/calendar/components/block-bucket.tsx
@@ -3,7 +3,7 @@ import { DAY_COL_MIN_W, MOCK_EVENTS } from "../constants";
 
 export function BlockBucket() {
   return (
-    <div className="flex flex-row lg:flex-col shrink-0 pt-grid-gap px-grid-gap lg:px-0 lg:py-grid-gap pb-grid-gap lg:pl-grid-gap bg-canvas">
+    <div className="flex flex-row lg:flex-col shrink-0 pt-grid-gap lg:py-grid-gap pb-grid-gap lg:pl-grid-gap bg-canvas px-grid-gap lg:px-0">
       <div className="flex flex-row lg:flex-col rounded-xl bg-surface p-grid-gap gap-grid-gap h-full min-h-0 overflow-x-auto lg:overflow-x-hidden lg:overflow-y-auto">
         {MOCK_EVENTS.map((event) => (
           <div
