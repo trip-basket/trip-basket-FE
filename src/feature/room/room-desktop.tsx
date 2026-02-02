35ac454702084fe0aa25b97bbe7fae4ed9c1e271
Taewoong Heo <chamjoeun0111@gmail.com>
2026-02-02 15:01:30 +0900

    style: 레이아웃 크기 변경

--- src/feature/room/room-desktop.tsx
+++ src/feature/room/room-desktop.tsx
@@ -10,7 +10,7 @@ export function RoomDesktop() {
 
   return (
     <div className="flex flex-1 min-h-0 flex-row p-grid-gap overflow-hidden">
-      <div className="flex basis-3/5 shrink-0 min-w-[500px] min-h-0 flex-col overflow-hidden">
+      <div className="flex basis-3/6 shrink-0 min-w-[500px] min-h-0 flex-col overflow-hidden">
         <Calendar />
       </div>
 
